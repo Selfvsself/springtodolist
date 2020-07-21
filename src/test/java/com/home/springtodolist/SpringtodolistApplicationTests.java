f4d979ee89882e1a04de43a02d0c96b0454f490d
Kayumov Alexander <killerman323@gmail.com>
2020-07-21 10:38:41 +0300

    Initial commit

---
+++ src/test/java/com/home/springtodolist/SpringtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.springtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
